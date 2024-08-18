48d2b8399c02436a9218944e6f4115fc200408a4
bootleggers26@mail.ru <bootleggers26@mail.ru>
2024-08-18 23:39:28 +0300

    header

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Portfolio_5-Archi-Web
+
 Portfolio_5-Archi Web
+![Portfolio_5-Archi Web](img/pixelPerfect.jpg)
---
+++ js/script.js
@@ -0,0 +1,13 @@
+const burgerMenu = document.querySelector(".burger-menu");
+const menuModal = document.querySelector(".menu-modal");
+
+burgerMenu.addEventListener("click", () => {
+  menuModal.style.display =
+    menuModal.style.display === "block" ? "none" : "block";
+});
+
+document.addEventListener("click", (e) => {
+  if (!burgerMenu.contains(e.target) && !menuModal.contains(e.target)) {
+    menuModal.style.display = "none";
+  }
+});
